97b3225c56c03a3a198093f7a2b3a3bc512bfed7
Marco Boncoraglio <marcoboncoraglio@gmail.com>
2019-09-18 18:20:08 +0200

    added semicolons, looked unsettling without them

--- backend/routes/friendListsRoutes.js
+++ backend/routes/friendListsRoutes.js
@@ -1,81 +1,81 @@
-const express = require('express')
-const router = express.Router()
-const FriendLists = require('../models/friendLists')
+const express = require('express');
+const router = express.Router();
+const FriendLists = require('../models/friendLists');
 
 router.get('/', async (req, res) => {
-  const friendLists = await FriendLists.find()
-  res.json(friendLists)
-})
+  const friendLists = await FriendLists.find();
+  res.json(friendLists);
+});
 
 router.get('/:id', getFriendLists, (req, res) => {
-  res.json(res.friendLists)
-})
+  res.json(res.friendLists);
+});
 
 router.post('/', async (req, res) => {
   const friendLists = new FriendLists({
     friendList: req.body.friendList,
     friendRequests: req.body.friendRequests
-  })
+  });
 
   try {
-    const newFriendLists = await friendLists.save()
-    res.status(201).json(newFriendLists)
+    const newFriendLists = await friendLists.save();
+    res.status(201).json(newFriendLists);
   } catch (err) {
-    res.status(400).json({ message: err.message })
+    res.status(400).json({ message: err.message });
   }
-})
+});
 
 router.put('/:id', getFriendLists, async (req, res) => {
-  res.friendLists.friendList = req.body.friendList
-  res.friendLists.friendRequests = req.body.friendRequests
+  res.friendLists.friendList = req.body.friendList;
+  res.friendLists.friendRequests = req.body.friendRequests;
 
   try {
-    const updatedFriendLists = await res.friendLists.save()
-    res.json(updatedFriendLists)
+    const updatedFriendLists = await res.friendLists.save();
+    res.json(updatedFriendLists);
   } catch{
-    res.status(400).json({ message: err.message })
+    res.status(400).json({ message: err.message });
   }
-})
+});
 
 router.patch('/:id', getFriendLists, async (req, res) => {
   if (req.body.friendList != null) {
-    res.friendLists.friendList = req.body.friendList
+    res.friendLists.friendList = req.body.friendList;
   }
 
   if (req.body.friendRequests != null) {
-    res.friendLists.friendRequests = req.body.friendRequests
+    res.friendLists.friendRequests = req.body.friendRequests;
   }
 
   try {
-    const updatedFriendList = await res.friendLists.save()
-    res.json(updatedFriendList)
+    const updatedFriendList = await res.friendLists.save();
+    res.json(updatedFriendList);
   } catch {
-    res.status(400).json({ message: err.message })
+    res.status(400).json({ message: err.message });
   }
-})
+});
 
 router.delete('/:id', getFriendLists, async (req, res) => {
   try {
-    await res.friendLists.remove()
-    res.json({ message: 'Deleted friendLists' })
+    await res.friendLists.remove();
+    res.json({ message: 'Deleted friendLists' });
   } catch (err) {
-    res.status(500).json({ message: err.message })
+    res.status(500).json({ message: err.message });
   }
-})
+});
 
 async function getFriendLists(req, res, next) {
   try {
-    const friendLists = await FriendLists.findById(req.params.id)
+    const friendLists = await FriendLists.findById(req.params.id);
     if (friendLists == null) {
-      return res.status(404).json({ message: 'Cant find friend lists' })
+      return res.status(404).json({ message: 'Cant find friend lists' });
     }
     
-    res.friendLists = friendLists
-    next()
+    res.friendLists = friendLists;
+    next();
     
   } catch (err) {
-    return res.status(500).json({ message: err.message })
+    return res.status(500).json({ message: err.message });
   }
 }
 
-module.exports = router
+module.exports = router;
--- backend/routes/locationRoutes.js
+++ backend/routes/locationRoutes.js
@@ -1,82 +1,82 @@
-const express = require('express')
-const router = express.Router()
-const Location = require('../models/location')
+const express = require('express');
+const router = express.Router();
+const Location = require('../models/location');
 
 //TODO: allow getting locations withing a certain radius
 router.get('/', async (req, res) => {
-  const locations = await Location.find()
-  res.json(locations)
-})
+  const locations = await Location.find();
+  res.json(locations);
+});
 
 router.get('/:id', getLocation, (req, res) => {
-  res.json(res.location)
-})
+  res.json(res.location);
+});
 
 router.post('/', async (req, res) => {
   const location = new Location({
     lat: req.body.lat,
     lng: req.body.lng
-  })
+  });
 
   try {
-    const newLocation = await location.save()
-    res.status(201).json(newLocation)
+    const newLocation = await location.save();
+    res.status(201).json(newLocation);
   } catch (err) {
-    res.status(400).json({ message: err.message })
+    res.status(400).json({ message: err.message });
   }
-})
+});
 
 router.put('/:id', getLocation, async (req, res) => {
-  res.location.lat = req.body.lat
-  res.location.lng = req.body.lng
+  res.location.lat = req.body.lat;
+  res.location.lng = req.body.lng;
 
   try {
-    const updatedLocation = await res.location.save()
-    res.json(updatedLocation)
+    const updatedLocation = await res.location.save();
+    res.json(updatedLocation);
   } catch{
-    res.status(400).json({ message: err.message })
+    res.status(400).json({ message: err.message });
   }
-})
+});
 
 router.patch('/:id', getLocation, async (req, res) => {
   if (req.body.lat != null) {
-    res.location.lat = req.body.lat
+    res.location.lat = req.body.lat;
   }
 
   if (req.body.lng != null) {
-    res.location.lng = req.body.lng
+    res.location.lng = req.body.lng;
   }
 
   try {
-    const updatedLocation = await res.location.save()
-    res.json(updatedLocation)
+    const updatedLocation = await res.location.save();
+    res.json(updatedLocation);
   } catch {
-    res.status(400).json({ message: err.message })
+    res.status(400).json({ message: err.message });
   }
-})
+});
 
 router.delete('/:id', getLocation, async (req, res) => {
   try {
-    await res.location.remove()
-    res.json({ message: 'Deleted Location' })
+    await res.location.remove();
+    res.json({ message: 'Deleted Location' });
   } catch (err) {
-    res.status(500).json({ message: err.message })
+    res.status(500).json({ message: err.message });
   }
-})
+});
 
 async function getLocation(req, res, next) {
   try {
-    const location = await Location.findById(req.params.id)
+    const location = await Location.findById(req.params.id);
     if (location == null) {
-      return res.status(404).json({ message: 'Cant find location' })
+      return res.status(404).json({ message: 'Cant find location' });
     }
     
-    res.location = location
-    next()
+    res.location = location;
+    next();
     
   } catch (err) {
-    return res.status(500).json({ message: err.message })
+    return res.status(500).json({ message: err.message });
   }
 }
 
-module.exports = router
+module.exports = router;
