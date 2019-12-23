1b67833e9956bb0e6f4d10096580d4fd263046c3
mboncoraglio <marcoboncoraglio@gmail.com>
2019-12-23 16:50:57 +0100

    porting project to frontend only, backend implemented in separate project

--- backend/passport-config/userLocalConfig.js
+++
@@ -1,8 +0,0 @@
-const UserLogin = require('../models/userLogin');
-
-module.exports = function loadUserConfig(passport){
-    passport.use(UserLogin.createStrategy());
-    passport.serializeUser(UserLogin.serializeUser());
-    passport.deserializeUser(UserLogin.deserializeUser());
-}
-
