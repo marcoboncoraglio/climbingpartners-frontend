b22b3eb4ac8c8f161fb9d8b5799af475c07518b9
Marco Boncoraglio <marcoboncoraglio@gmail.com>
2020-11-03 19:07:58 +0100

    update userStore to use new api

--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -1,144 +1,84 @@
-import { EventEmitter } from "events";
-import dispatcher from "../dispatcher";
-import { db } from "../firebase/Firebase";
-import { IUserDetails, IUserCard } from "../interfaces/UserInterfaces";
+import { EventEmitter } from 'events';
+import dispatcher from '../dispatcher';
+import { IUserDetails, IUserCard } from '../interfaces/UserInterfaces';
+const axios = require('axios');
 
 class UserStore extends EventEmitter {
-  uid: string = "0";
+  uid: string = '0';
 
   details: IUserDetails = {
     birthday: new Date(1995, 11, 17),
-    about: "Hi there!",
-    climbingStyles: ["Lead", "Bouldering"],
-    availableEquipment: ["60m Rope"],
-    languagesSpoken: ["English"]
+    about: 'Hi there!',
+    climbingStyles: ['Lead', 'Bouldering'],
+    availableEquipment: ['60m Rope'],
+    languagesSpoken: ['English'],
   };
 
   card: IUserCard = {
-    name: "",
-    imgUrl: ""
+    name: '',
+    imgUrl: '',
   };
+
+  url: string = process.env.BACKEND_URL_TEST || 'localhost:4000';
 
   getId(): string {
     return this.uid;
   }
 
-  getDetails(uid: string) {
+  getDetails(uid?: string) {
     if (!uid) {
-      return new Promise(res => {
+      return new Promise((res) => {
         res(this.details);
       });
     } else {
-      return new Promise(res => {
-        db.ref()
-          .child("details/" + uid)
-          .on("value", snap => {
-            if (snap != null) res(snap.val());
-          });
+      return new Promise((res) => {
+        axios.get(`{$url}/userDetails/{$uid}`).then((json) => res(json));
       });
     }
   }
 
-   getCard(uid: string) {
+  getCard(uid?: string) {
     if (!uid) {
       return this.card;
     } else {
-      return new Promise(res => {
-        db.ref()
-          .child("cards/" + uid)
-          .on("value", snap => {
-            if (snap != null && snap.val() != null) {
-              res(snap.val());
-            }
-          });
+      return new Promise((res) => {
+        axios.get(`{$url}/userCards/{$uid}`).then((json) => res(json));
       });
     }
   }
 
-  getName(uid: string) {
-    if (!uid) return this.uid;
-    else {
-      return new Promise(res => {
-        db.ref()
-          .child("cards/" + uid)
-          .on("value", snap => {
-            if (snap != null) res(snap.val().name);
-          });
-      });
-    }
-  }
-
-  //TODO: dont want to pass entire user, just properties
   setDetails(details: IUserDetails) {
-    this.details = details;
-    db.ref()
-      .child("details/" + this.uid)
-      .set(this.details);
-    this.emit("change_details");
+    axios
+      .put(`{$url}/userDetails/{$uid}`)
+      .then((updatedDetails: IUserDetails) => (this.details = updatedDetails));
+
+    this.emit('change_details');
   }
 
   setCard(card: IUserCard) {
-    this.card = card;
-    db.ref()
-      .child("cards/" + this.uid)
-      .set(this.card);
-    this.emit("change_card");
-  }
+    axios
+      .put(`{$url}/userCard/{$uid}`)
+      .then((updatedCard: IUserCard) => (this.card = updatedCard));
 
-  setName(name: string) {
-    this.card.name = name;
-    db.ref()
-      .child("cards/" + this.uid)
-      .set(this.card);
-    this.emit("change_card");
+    this.emit('change_card');
   }
-
-  setImg(img: string) {
-    this.card.imgUrl = img;
-    db.ref()
-      .child("cards/" + this.uid)
-      .set(this.card);
-    this.emit("change_card");
-  }
-
-  //TODO: get and use imgUrl
   onLogin(userObj: any) {
     this.uid = userObj.uid;
-
-    if (!this.card.name) {
-      this.setName(userObj.displayName);
-    }
-
-    db.ref()
-      .child("details/" + this.uid)
-      .on("value", snap => {
-        if (snap != null) {
-          this.details = Object.assign({}, this.details, snap.val());
-          this.setDetails(this.details);
-        }
-      });
-
-    db.ref()
-      .child("cards/" + this.uid)
-      .on("value", snap => {
-        if (snap != null) {
-          this.card = Object.assign({}, this.card, snap.val());
-          this.setCard(this.card);
-        }
-      });
+    this.getCard();
+    this.getDetails();
   }
 
   handleActions = (action: any) => {
     switch (action.type) {
-      case "EDIT_DETAILS": {
+      case 'EDIT_DETAILS': {
         this.setDetails(action.details);
         break;
       }
-      case "EDIT_CARD": {
+      case 'EDIT_CARD': {
         this.setCard(action.card);
         break;
       }
-      case "LOGIN": {
+      case 'LOGIN': {
         this.onLogin(action.uObject);
         break;
       }
