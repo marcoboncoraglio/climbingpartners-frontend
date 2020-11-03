34d528b57bb93d6c5a79bf1d6bc983c13a0d1a88
Marco Boncoraglio <marcoboncoraglio@gmail.com>
2020-11-03 19:15:10 +0100

    update locationStore to new api

--- src/stores/LocationStore.ts
+++ src/stores/LocationStore.ts
@@ -1,85 +1,100 @@
-import { EventEmitter } from "events";
-import dispatcher from "../dispatcher";
-import { db } from "../firebase/Firebase";
-import { ILocation } from "../interfaces/LocationInterfaces";
+import { EventEmitter } from 'events';
+import dispatcher from '../dispatcher';
+import { ILocation } from '../interfaces/LocationInterfaces';
+const axios = require('axios');
 
 class LocationStore extends EventEmitter {
-    uid: string = "0";
-    
-    location: ILocation = {
-        lat: 0,
-        lng: 0
-    }
-    
-    gpsID: number = 0;
+  uid: string = '0';
 
-    highAccuracy: boolean = false;
+  location: ILocation = {
+    lat: 0,
+    lng: 0,
+  };
 
-    getId(){
-        return this.uid;
-    }
+  gpsID: number = 0;
 
-    onLogin(userObject: any) {
-        this.uid = userObject.uid;
-    }
+  highAccuracy: boolean = false;
 
-    setLocation(location: ILocation) {
-        db.ref()
-            .child("locations/" + this.uid)
-            .set(location);
-        this.emit("location");
-    }
+  url: string = process.env.BACKEND_URL_TEST || 'localhost:4000/api/';
 
-    getLocation() {
-        return new Promise((resolve) => {
-            navigator.geolocation.getCurrentPosition((position) => {
-                resolve({
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                });
-            }, (err) => {
-                console.log(err.message + ", getting approximate locationg from ip...")
-                resolve(fetch('https://ipapi.co/json')
-                    .then(res => res.json())
-                    .then(location => {
-                        return {
-                            lat: location.latitude,
-                            lng: location.longitude
-                        };
-                    }));
-            }, { enableHighAccuracy: this.highAccuracy });
-        });
-    }
+  getId() {
+    return this.uid;
+  }
 
-    enableGPS() {
-        this.gpsID = navigator.geolocation.watchPosition(
-            () => { this.highAccuracy = true },
-            () => { this.highAccuracy = false },
-            { enableHighAccuracy: this.highAccuracy }
-        );
-    }
+  onLogin(userObject: any) {
+    this.uid = userObject.uid;
+  }
 
-    disableGPS() {
-        navigator.geolocation.clearWatch(this.gpsID);
+  setLocation(location: ILocation) {
+    axios
+      .put(`{$url}/locations/{$uid}`)
+      .then((locationDetails: ILocation) => (this.location = locationDetails));
+
+    this.emit('location');
+  }
+
+  getLocation() {
+    return new Promise((resolve) => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          resolve({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+        },
+        (err) => {
+          console.log(
+            err.message + ', getting approximate locationg from ip...'
+          );
+          resolve(
+            fetch('https://ipapi.co/json')
+              .then((res) => res.json())
+              .then((location) => {
+                return {
+                  lat: location.latitude,
+                  lng: location.longitude,
+                };
+              })
+          );
+        },
+        { enableHighAccuracy: this.highAccuracy }
+      );
+    });
+  }
+
+  enableGPS() {
+    this.gpsID = navigator.geolocation.watchPosition(
+      () => {
+        this.highAccuracy = true;
+      },
+      () => {
         this.highAccuracy = false;
-        this.gpsID = 0;
-    }
+      },
+      { enableHighAccuracy: this.highAccuracy }
+    );
+  }
 
-    handleActions = (action: any) => {
-        switch (action.type) {
-            case "SET_LOCATION": {
-                this.setLocation(action.location);
-                break;
-            }
-            case "LOGIN": {
-                this.onLogin(action.uObject);
-                break;
-            }
-            default: {
-                return;
-            }
-        }
-    };
+  disableGPS() {
+    navigator.geolocation.clearWatch(this.gpsID);
+    this.highAccuracy = false;
+    this.gpsID = 0;
+  }
+
+  handleActions = (action: any) => {
+    switch (action.type) {
+      case 'SET_LOCATION': {
+        this.setLocation(action.location);
+        break;
+      }
+      case 'LOGIN': {
+        this.onLogin(action.uObject);
+        break;
+      }
+      default: {
+        return;
+      }
+    }
+  };
 }
 
 const locationStore = new LocationStore();
