3787b22c7a2af895675ef9ff378432723da1ba3e
Marco Boncoraglio <marcoboncoraglio@gmail.com>
2020-11-03 19:06:07 +0100

    remove firebase dbs, add dotenv

--- src/firebase/Firebase.js
+++
@@ -1,7 +0,0 @@
-import firebase from 'firebase/app';
-import 'firebase/database';
-import {config} from './config';
-
-export const app = firebase.initializeApp(config);
-export const db = app.database();
-
--- src/index.js
+++ src/index.js
@@ -6,34 +6,39 @@ import * as serviceWorker from './serviceWorker';
 import 'typeface-roboto';
 
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import { AppTheme } from './theme'
+import { AppTheme } from './theme';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
-import { transitions, positions, Provider as AlertProvider } from 'react-alert'
-import AlertTemplate from 'react-alert-template-basic'
+import { transitions, positions, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+
+require('dotenv').config();
 
 const options = {
-    position: positions.BOTTOM_CENTER,
-    timeout: 5000,
-    offset: '30px',
-    transition: transitions.SCALE
-  }
-  
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: '30px',
+  transition: transitions.SCALE,
+};
 
 const Index = () => {
-    return (
-        <MuiThemeProvider theme={AppTheme}>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                <AlertProvider template={AlertTemplate} {...options}>
-                    <App />
-                </AlertProvider>
-            </MuiPickersUtilsProvider>
-        </MuiThemeProvider>
-    );
-}
+  return (
+    <MuiThemeProvider theme={AppTheme}>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <AlertProvider template={AlertTemplate} {...options}>
+          <App />
+        </AlertProvider>
+      </MuiPickersUtilsProvider>
+    </MuiThemeProvider>
+  );
+};
 
 ReactDOM.render(
-    <React.StrictMode><Index /></React.StrictMode>, document.getElementById('root'));
+  <React.StrictMode>
+    <Index />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 //ReactDOM.createRoot(document.getElementById('root')).render(<Index />);
 
